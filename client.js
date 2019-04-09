5e36329c5d075e0479c73a7d3c7928a5d990d206
Peter Kotowski <bighoopee@gmail.com>
2019-04-09 11:10:28 -0700

    step5

--- client.js
+++ client.js
@@ -1,7 +1,22 @@
 const net = require('net');
+const readLine = require('readLine');
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> '
+});
+
+rl.prompt();
+rl.on('line', line => {
+  console.log(line);
+  rl.prompt();
+});
 
 const client = net.createConnection(7890, 'local', () => {
   console.log('im connected');
+    
+
   client.write('hi im a client');
 });
 
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const net = require('net');
 
 const server = net.createServer((client) => {
   console.log('client created');
-  client.on('data', data => {
-      console.log(data.toString());
-  });
+  //   client.on('data', data => {
+  //     //   console.log(data.toString());
+  //   });
+  client.pipe(client);
+
 });
 
 server.listen(7890);
